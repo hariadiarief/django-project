129b1c0e8f5d32f2a12c1ea3b9d3394450abdd13
hariadiarief <adi.arief77@gmail.com>
2023-06-08 13:17:23 +0700

    ep: membuat hello world

--- webapp/about/views.py
+++ webapp/about/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index( request): 
+    context = {
+        'nama': 'hello world',
+    }
+
+    return render(request, 'index.html', context)
