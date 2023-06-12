092d66283f9aeacb670487d34ab9e7de0e27dd40
hariadiarief <adi.arief77@gmail.com>
2023-06-12 22:23:54 +0700

    ep15: custom login and dashboard page

--- webapp/login/urls.py
+++ webapp/login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login')
+    path('', views.login_view, name='login'),
+    path('dashboard/', views.dashboard, name='login')
 ]
--- webapp/login/views.py
+++ webapp/login/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import FormLogin
 
 # Create your views here.
 
 
-def login(request):
-    context = {
-        'name': 'login'
-    }
+def login_view(request):
+    form = FormLogin()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-    return render(request, 'login/login.html', context)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/login/dashboard')
+
+    return render(request, 'login/login.html', {'form': form})
+
+
+def dashboard(request):
+    context = {}
+    return render(request, 'login/dashboard.html', context)
