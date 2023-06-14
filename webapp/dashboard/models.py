76277991f990df46fce5f5ae2e237e95be7dbb65
hariadiarief <adi.arief77@gmail.com>
2023-06-14 09:38:35 +0700

    ep26: Pengenalan Models Django

--- webapp/dashboard/models.py
+++ webapp/dashboard/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
 # Create your models here.
+
+
+class employee(models.Model):
+    name = models.CharField(max_length=255)
+    adress = models.CharField(max_length=225)
+    GENDER = (
+        ('laki-laki', 'laki-laki'),
+        ('perempuan', 'perempuan'),
+    )
+    gender = models.CharField(max_length=225, choices=GENDER)
+
+    EMPLOYEE_TYPE = (
+        ('pembina - I/A', 'pembina - I/A'),
+        ('pembina - II/A', 'pembina - II/A'),
+        ('pembina - III/A', 'pembina - I/IIA'),
+        ('pembina - II/B', 'pembina - II/B'),
+        ('penata - I/A', 'penata - I/A'),
+        ('penata - II/A', 'penata - II/A'),
+        ('penata - II/B', 'penata - II/B'),
+        ('penata - III/C', 'penata - IIICA'),
+    )
+
+    employeeType = models.CharField(max_length=50, choices=EMPLOYEE_TYPE)
+
+    def __str__(self):
+        return self.name
