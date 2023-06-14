093922dc04e028ea66f45b994dcdc3c2cb1b8ca4
hariadiarief <adi.arief77@gmail.com>
2023-06-14 10:08:11 +0700

    ep27: List Display Data Pegawai Django Administrator

--- webapp/dashboard/admin.py
+++ webapp/dashboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from dashboard.models import employee
 
 # Register your models here.
+
+# (option:1) untuk memunculkan table sederhana
+# admin.site.register(employee)
+
+# (option:2) untuk memunculkan custom table
+
+
+@admin.register(employee)
+class employeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'adress', 'gender', 'employeeType')
